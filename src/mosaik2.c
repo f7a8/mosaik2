20d8808b1b91324c498810641d88664aa95b49f4
f7a8 <f7a7@web.de>
2022-04-17 21:39:42 +0200

    using new mosaik2_indextask struct; removed tiler as function, because now it called directly from the index process.

--- src/mosaik2.c
+++ src/mosaik2.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <openssl/md5.h>
+#include <err.h>
 
 #include "mosaik2.h"
 
@@ -35,7 +37,7 @@ int main(int argc, char **argv) {
 			exit(EXIT_FAILURE);
 		}
 		return mosaik2_index(argv[2], atoi(argv[3]), atoi(argv[4]));
-	} else if(strncmp(	argv[1], tiler, strlen(tiler)) == 0) {
+	/*} else if(strncmp(	argv[1], tiler, strlen(tiler)) == 0) {
 	
 		if(argc!=4) {
 			fprintf(stderr,"wrong parameter. usage param 1=> tile_count, 2=> file_size of the image in bytes. Image data is only accepted via stdin stream\n");
@@ -43,7 +45,12 @@ int main(int argc, char **argv) {
 		}
 		uint32_t tile_count = atoi(argv[2]);
 		uint32_t file_size = atoi(argv[3]);
-		return mosaik2_tiler(tile_count,file_size);
+		
+		mosaik2_indextask task;
+		task.tile_count = atoi(argv[2]);
+		task.filesize = atoi(argv[3]);
+
+		return mosaik2_tiler(NULL);*/
 	} else if(strncmp( argv[1], gathering, strlen(gathering)) == 0) {
 
 		if(argc!=8) {
