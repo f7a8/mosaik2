81089ed9c7d01d08faf0aea5edd4c54467444ead
f7a8 <f7a7@web.de>
2022-04-17 21:35:59 +0200

    introduced new mosaik2_indextask struct; change data type long double to double for colors and stddev, which reducued the calculation time by factor 2; removed the stdout prints of the result, now the result data is save within the mosaik2_indextask struct and its passed to the function mosaik2_index_write_to_disk

--- src/tiler.c
+++ src/tiler.c
@@ -2,8 +2,16 @@
 #include "libmosaik2.h"
 
 
-int mosaik2_tiler(uint32_t tile_count, uint32_t file_size) {
+int mosaik2_tiler(struct mosaik2_database_struct *md, mosaik2_indextask *task) {
+//	print_usage("t read0");
+	if( mosaik2_indextask_read_image(task) ) {
+		fprintf(stderr, "could not read image\n");
+		exit(EXIT_FAILURE);
+	}
+	//print_usage("t read1");
 
+	uint8_t tile_count = md->tilecount;
+	uint32_t file_size = task->filesize;
 	check_resolution(tile_count);
 
 	if(file_size < 10001) {
@@ -14,58 +22,29 @@ int mosaik2_tiler(uint32_t tile_count, uint32_t file_size) {
 	int debug=0;
 	int debug1=0;
 	int html = 0;
-	int out = 1;
-
-
-	if(debug) printf("tile_count:%i,file_size:%i\n",tile_count,file_size);
-
-  unsigned char *buffer = malloc(file_size);
-  if(!buffer) {
-    fprintf(stderr,"memory (%i) could not be allocated\n", file_size);
-    exit(EXIT_FAILURE);
-  }
-
-
-//  clock_t start, end;
-//  start = clock();
-
-  size_t bytes_read = fread(buffer, 1, file_size, stdin); //works
-  
-	if(bytes_read != file_size) {
-    fprintf(stderr, "image could not be loaded bytes_should:%i, bytes_read:%li", file_size, bytes_read);
-    exit(EXIT_FAILURE);
-  }
-	
-	char buf2[1];
-	if(fread(buf2, 1,1, stdin)>0) {
-		fprintf(stderr, "after file_size (%i) is at least one byte further more to read\n", file_size);
-		exit(EXIT_FAILURE);
-	}
-	
-	fclose(stdin);
+	int out  = 0;
+	int ii=0;
+  unsigned char *buffer = task->image_data;
 
 	int file_type = get_file_type_from_buf(buffer,file_size);
 	if(file_type == FT_ERR) {
 		fprintf(stderr, "illegal image type 0\n");
 		exit(EXIT_FAILURE);
 	}
-//  end = clock();
-//  printf( "fread cpu time used: %f\n", ((double) (end-start)) / CLOCKS_PER_SEC );
 
-
+	//print_usage("t_create0");
   gdImagePtr im;
   if(file_type == FT_JPEG) 
 		im = gdImageCreateFromJpegPtrEx(file_size, buffer, 0);
-	//else if(file_type == FT_PNG)
-	//	im = gdImageCreateFromPngPtr(file_size, buffer);
 	else {
 		fprintf(stderr, "illegal image type 1\n");
 		exit(EXIT_FAILURE);
 	}
+	//print_usage("t_create1");
 
 	if(im == NULL ) {
   	free(buffer);
-
+		
 		fprintf(stderr,"image could not be instanciated\n");
 		exit(EXIT_FAILURE);
 	}
@@ -88,13 +67,20 @@ int mosaik2_tiler(uint32_t tile_count, uint32_t file_size) {
 		im = im2;
 	}
 
+//	print_usage("t_orientat");
+
 	int width = gdImageSX(im);
 	int height = gdImageSY(im);
+
+	task->width = width;
+	task->height = height;
+
+	
 
 	if(width < tile_count || height < tile_count) {
   	free(buffer);
 		gdImageDestroy(im);
-
+	
 		fprintf(stderr,"image is too small, at least one dimension is smaller than the tile_count\n");
 		exit(EXIT_FAILURE);
 	}
@@ -117,6 +103,7 @@ int mosaik2_tiler(uint32_t tile_count, uint32_t file_size) {
 	}
 
 	int total_tile_count = tile_x_count * tile_y_count;
+	task->total_tile_count = total_tile_count;
   
 	if( tile_x_count >= 256 || tile_y_count >= 256 ) {
   	free(buffer);
@@ -126,19 +113,15 @@ int mosaik2_tiler(uint32_t tile_count, uint32_t file_size) {
 		exit(EXIT_FAILURE);
 	}
 
+	task->tile_x_count = tile_x_count;
+	task->tile_y_count = tile_y_count;
 
-	unsigned char hash[MD5_DIGEST_LENGTH];
-	memset(hash,0,MD5_DIGEST_LENGTH);
-	//for(int i=0;i<16;i++) digest[i]=0;
 	MD5_CTX md5;
 	MD5_Init (&md5);
 	MD5_Update (&md5, buffer, file_size);
-	MD5_Final ( hash, &md5);
-	//
-//void md5_hash(const uint8_t message[], size_t len, uint32_t hash[static STATE_LEN]);
-	//uint32_t hash[STATE_LEN] ={0};
-	//md5_hash( buffer, file_size, hash );
-	//if(debug1) fprintf(stderr, "state_len:%i\n",hash[0]);
+	MD5_Final ( task->hash, &md5);
+
+	//print_usage("t_diggest");
 
 
 	//	if(out) printf(" %02X%02X%02X %02X%02X%02X",red,green,blue, (int)round(abw_red),(int)round(abw_green),(int)round(abw_blue));
@@ -156,28 +139,26 @@ int mosaik2_tiler(uint32_t tile_count, uint32_t file_size) {
 
 	if(out) printf("%04X %04X %02X %02X", width, height, tile_x_count, tile_y_count);
 	if(debug1) fprintf(stderr,"gathering imagedata\n");
-    
-	double      colors_red      [total_tile_count];
-	double      colors_green    [total_tile_count];
-	double      colors_blue     [total_tile_count];
-	long double colors_abw_red  [total_tile_count];
-	long double colors_abw_green[total_tile_count];
-	long double colors_abw_blue [total_tile_count];
+  
+// void *calloc(size_t nmemb, size_t size);
+	double colors_red[total_tile_count];
+	double colors_green[total_tile_count];
+	double colors_blue[total_tile_count];
+	double colors_stddev_red[total_tile_count];
+	double colors_stddev_green[total_tile_count];
+	double colors_stddev_blue[total_tile_count];
+	//print_usage("t_stackmem");
 
+	task->colors = calloc(3*total_tile_count, sizeof(uint8_t));
+	if(task->colors==NULL) { fprintf(stderr, "cannot allocate memory in tiler for colors\n"); exit(1); }
+	task->colors_stddev = calloc(3*total_tile_count, sizeof(uint8_t));
+	if(task->colors_stddev==NULL) { fprintf(stderr, "cannot allocate memory in tiler for colors_stddev\n"); exit(1); }
+	//print_usage("t_calloc");
 
-	for(int i=0;i<total_tile_count;i++) {
-		colors_red[i]=0.0;
-		colors_green[i]=0.0;
-		colors_blue[i]=0.0;
-		colors_abw_red[i]=0.0;
-		colors_abw_green[i]=0.0;
-		colors_abw_blue[i]=0.0;
-	}
-
+	int red0, green0, blue0;
 		for(int j=0,j1=offset_y;j1<ly;j++,j1++){    
            //printf("%i\n",i1); 
 			for(int i=0,i1=offset_x;i1<lx;i++,i1++){
-            
 			//i and j are running from zero to the length of the cropped area
 			//i1 and j1 are dealing with the corrected offset, so they are pointig to the valid pixels
 
@@ -186,9 +167,9 @@ int mosaik2_tiler(uint32_t tile_count, uint32_t file_size) {
 			int tile_idx = tile_y * tile_x_count + tile_x;
 			int color =  gdImageTrueColorPixel(im,i1,j1);
 			//TODO one function 
-			int red0 =   gdTrueColorGetRed(color);
-			int green0 = gdTrueColorGetGreen(color);
-			int blue0 =  gdTrueColorGetBlue(color);
+			red0 =   gdTrueColorGetRed(color);
+			green0 = gdTrueColorGetGreen(color);
+			blue0 =  gdTrueColorGetBlue(color);
 			//int red0 = color >> 16 & 0xff;;
 			//int green0 = color >> 8 & 0xff;;
 			//int blue0 = color & 0xff;
@@ -202,47 +183,52 @@ int mosaik2_tiler(uint32_t tile_count, uint32_t file_size) {
 		}
 	}
     
+//	print_usage("t_colors");
 	if(debug1) printf("avg_blue0:%f \n", colors_blue[0]);        
 
 
 
 	if(debug1) fprintf(stderr,"tiling stddev data\n");
 
+	double red2, green2, blue2;
+
 	//double total_tile_count_d = total_tile_count;
 	for(int j=0,j1=offset_y;j1<ly;j++,j1++){
 		for(int i=0,i1=offset_x;i1<lx;i++,i1++){
 			//if(debug) printf("avg_blue0:%f i:%i i1:%i lx:%i\n", colors_blue[0],i ,i1, lx);        
-
+			
 			int tile_x = i/pixel_per_tile;
 			int tile_y = j/pixel_per_tile;
 			int tile_idx = tile_y * tile_x_count + tile_x;
-		if(debug) printf( "i:%i j:%i, tile_x:%i tile_y:%i tile_idx:%i c:%f %f %f\n", i,j,tile_x, tile_y, tile_idx, colors_red[tile_idx], colors_green[tile_idx],colors_blue[tile_idx]);
+		if(debug) printf( "i:%i j:%i, tile_x:%i tile_y:%i tile_idx:%i c:%f %f %f\n", i,j,tile_x, tile_y, tile_idx, colors_red[tile_idx], colors_green[tile_idx], colors_blue[tile_idx]);
 			int color =  gdImageTrueColorPixel(im,i1,j1);
 
 			int red0 =   gdTrueColorGetRed(color);
 			int green0 = gdTrueColorGetGreen(color);
 			int blue0 =  gdTrueColorGetBlue(color);
-			//int red0 = color >> 16 & 0xff;;
-			//int green0 = color >> 8 & 0xff;;
-			//int blue0 = color & 0xff;
+			//red0 = color >> 16 & 0xff;;
+			//green0 = color >> 8 & 0xff;;
+			//blue0 = color & 0xff;
 
-			double red2 = colors_red[tile_idx] - red0;
-			double green2 = colors_green[tile_idx] - green0;
-			double blue2 = colors_blue[tile_idx] - blue0;
+			red2 = colors_red[tile_idx] - red0;
+			green2 = colors_green[tile_idx] - green0;
+			blue2 = colors_blue[tile_idx] - blue0;
 
-			colors_abw_red[tile_idx] += red2 * red2;
-			colors_abw_green[tile_idx] += green2 * green2;
-      colors_abw_blue[tile_idx] += blue2 * blue2;
+			colors_stddev_red[tile_idx] += pow(red2,2);// * red2;
+			colors_stddev_green[tile_idx] += pow(green2,2);// * green2;
+      colors_stddev_blue[tile_idx] += pow(blue2,2);// * blue2;
 		}
 	}
+	//print_usage("t_stddev");
 
-
+	gdImageDestroy(im);
 
 	if(debug1)printf("dim:%i %i,off:%i %i,l:%i %i,tile_count:%i %i,pixel_per_tile:%i %f\n",width,height,offset_x,offset_y,lx,ly,tile_x_count,tile_y_count,pixel_per_tile,total_pixel_per_tile);
 	if(html)printf("<html><head><style>table{ width:851px;height:566px; border-collapse: collapse;}td{padding:0;height:0.2em;width:0.2em;}</style></head><body><table>");
 
 
 	if(debug1)fprintf(stderr,"starting output\n");
+	double stddev_red, stddev_green, stddev_blue;
 
 	for(int y=0;y<tile_y_count;y++) {
 		if(html)printf("<tr>");
@@ -253,47 +239,44 @@ int mosaik2_tiler(uint32_t tile_count, uint32_t file_size) {
 			if(debug1) fprintf(stderr,"%i ", i);
 
 			if(debug) printf("avg_blue0:%f ", colors_blue[i]);
-			
-			int red =   (int)round(colors_red[i]);
-			int green = (int)round(colors_green[i]);
-			int blue =  (int)round(colors_blue[i]);
 
-			if(debug) printf("avg_red:%i %f", red,colors_red[i]);
-			if(debug) printf("avg_green:%i %f", green,colors_green[i]);
-			if(debug) printf("avg_blue:%i %f", blue,colors_blue[i]);
+			red0 = (int) round(colors_red[i]);
+			green0 = (int) round(colors_green[i]);
+			blue0 = (int)round(colors_blue[i]);
 
-		  double abw_red = sqrt(colors_abw_red[i]/ ( total_pixel_per_tile - 1.0 ));
-		  double abw_green = sqrt(colors_abw_green[i]/ (total_pixel_per_tile - 1.0));
-		  double abw_blue = sqrt(colors_abw_blue[i]/ ( total_pixel_per_tile - 1.0));
+			task->colors[i*3] = red0;
+			task->colors[i*3+1] = green0;
+			task->colors[i*3+2] = blue0;
 
+			//if(debug) printf("avg_red:%i %f", red,colors_red[i]);
+			//if(debug) printf("avg_green:%i %f", green,colors_green[i]);
+			//if(debug) printf("avg_blue:%i %f", blue,colors_blue[i]);
+
+		  stddev_red = sqrt(colors_stddev_red[i]/ ( total_pixel_per_tile - 1.0 ));
+		  stddev_green = sqrt(colors_stddev_green[i]/ (total_pixel_per_tile - 1.0));
+		  stddev_blue = sqrt(colors_stddev_blue[i]/ ( total_pixel_per_tile - 1.0));
 			// to better utilize the byte space
-			abw_red = abw_red * 2;
-			abw_green = abw_green * 2;
-			abw_blue*= abw_blue * 2;
+			// in most cases below 255 ..
+		  task->colors_stddev[i*3]   = stddev_red   * 2 > 255 ? 255 : stddev_red   * 2;
+		  task->colors_stddev[i*3+1] = stddev_green * 2 > 255 ? 255 : stddev_green * 2;
+		  task->colors_stddev[i*3+2] = stddev_blue  * 2 > 255 ? 255 : stddev_blue  * 2;
 			
-			// in most cases not needed..
-			if(abw_red>255) abw_red=255;
-			if(abw_green>255) abw_green=255;
-			if(abw_blue>255) abw_blue=255;
-			if(html)printf("<td style='background-color:#%02x%02x%02x'>",red,green,blue);
+			
+
+			if(html)printf("<td style='background-color:#%02x%02x%02x'>",red0,green0,blue0);
 //			if(html)printf("<td style='background-color:#%02x%02x%02x'>",(int)round(abw_red/2.0),(int)round(abw_green/2.0),(int)round(abw_blue/2.0));
-			if(debug)printf( "%i:%02X%02X%02X %i,%i,%i  %02X%02X%02X\n",i,red,green,blue,red,green,blue,(int)round(abw_red),(int)round(abw_green),(int)round(abw_blue));
-			if(out) printf(" %02X%02X%02X %02X%02X%02X",red,green,blue, (int)round(abw_red),(int)round(abw_green),(int)round(abw_blue));
+			if(debug)printf( "%i:%02X%02X%02X %i,%i,%i  %02X%02X%02X\n",i,red0,green0,blue0,red0,green0,blue0,(int)round(stddev_red),(int)round(stddev_green),(int)round(stddev_blue));
+			if(out) printf(" %02X%02X%02X %02X%02X%02X",red0,green0,blue0, (int)round(stddev_red),(int)round(stddev_green),(int)round(stddev_blue));
 			if(html)printf("</td>");
 		}
-	if(html)printf("</tr>");
+		if(html)printf("</tr>");
 	}
 	if(html)printf("</table></body></html>");
 	if(debug1) fprintf(stderr,"ending\n");
-	gdImageDestroy(im);
+	//print_usage("t_final");
 
-	if(out) printf(" %02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
-		hash[0]         , hash[1]         , hash[2]         , hash[3]         ,
-		hash[4]         , hash[5]         , hash[6]         , hash[7]         ,
-		hash[8]         , hash[9]         , hash[10]        , hash[11]        ,
-		hash[12]        , hash[13]        , hash[14]        , hash[15]        );
+	mosaik2_index_write_to_disk(md, task);
 
-	if(out) fflush(stdout);
 	return 0;
 
 }
