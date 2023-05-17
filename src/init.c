c206414550960c14c1de472126c4a2e1d1e001b6
f7a8 <f7a7@web.de>
2023-05-17 21:03:04 +0200

    print the database id when executing init mode

--- src/init.c
+++ src/init.c
@@ -2,33 +2,6 @@
 #include "libmosaik2.h"
 
 
-/*
- * Return a hex string representing the data pointed to by `p`,
- * converting `n` bytes.
- *
- * The string should be deallocated using `free()` by the caller.
- */
-char *phex(const void *p, size_t n)
-{
-    const unsigned char *cp = p;              /* Access as bytes. */
-    char *s = m_malloc(2*n + 1);       /* 2*n hex digits, plus NUL. */
-    size_t k;
-
-    for (k = 0; k < n; ++k) {
-        /*
-         * Convert one byte of data into two hex-digit characters.
-         */
-        sprintf(s + 2*k, "%02x", cp[k]);
-    }
-
-    /*
-     * Terminate the string with a NUL character.
-     */
-    s[2*n] = '\0';
-
-    return s;
-}
-
 /**
 	* create as new empty mosaik2 database file. In case of an error it exits the program.
   */
@@ -40,23 +13,18 @@ FILE* create_mosaik2_database_file(char *filename, uint8_t close, uint8_t print)
 	return f;
 }
 
-void create_mosaik2_database_file_id(char *filename, int print) {
-	FILE *file = create_mosaik2_database_file(filename, 0, 0);
+void create_mosaik2_database_file_id(mosaik2_database *md, int print) {
+	FILE *file = create_mosaik2_database_file(md->id_filename, 0, 0);
 	FILE *rand_file = m_fopen("/dev/random", "r");
 
-	unsigned char buf[8];
-	unsigned char buf2[17];
-	memset( buf, 0, 8 );
-	memset( buf2, 0, 17);
-
+	uint8_t buf[8];
 	m_fread(buf, 8, rand_file);
-	char *hexbuf = phex( buf, 8);
-	m_fwrite(hexbuf, 16, file);
-	free(hexbuf);
+	snprintf(md->id, md->id_len, "%02x%02x%02x%02x%02x%02x%02x%02x", buf[0], buf[1], buf[2], buf[3], buf[4], buf[5], buf[6], buf[7]);
+	m_fwrite(md->id, md->id_len, file);
 
 	m_fclose( file );
 	m_fclose( rand_file );
-	if(print) printf( "mosaik2 database file %s created\n", filename);
+	if(print) printf( "mosaik2 database file %s created\n", md->id_filename);
 }
 
 void create_mosaik2_database_file_readme(char *filename, int print) {
@@ -72,7 +40,7 @@ void create_mosaik2_database_file_val(char *filename, void *ptr, size_t len, int
 }
 
 int mosaik2_init(mosaik2_arguments *args) {
-	
+
 	char *mosaik2_database_name = args->mosaik2db;
 	int database_image_resolution = args->database_image_resolution;
 	time_t n = time(NULL);
@@ -88,7 +56,8 @@ int mosaik2_init(mosaik2_arguments *args) {
 		fprintf(stderr,"mosaik2 database directory (%s) could not be created: %s\n", mosaik2_database_name, strerror(errno));
 		exit(EXIT_FAILURE);
 	}
-	printf("mosaik2 database directory (%s) created\n", mosaik2_database_name);
+	create_mosaik2_database_file_id(&md, args->verbose);
+	printf("mosaik2 database directory (%s) created (id:%s)\n", mosaik2_database_name, md.id);
 
 	create_mosaik2_database_file(md.filesizes_filename, 1, args->verbose);
 	create_mosaik2_database_file(md.filenames_filename, 1, args->verbose);
@@ -109,12 +78,10 @@ int mosaik2_init(mosaik2_arguments *args) {
 	create_mosaik2_database_file(md.tileoffsets_filename, 1, args->verbose);
 	create_mosaik2_database_file(md.lastindexed_filename, 1, args->verbose);
 
-	create_mosaik2_database_file_id(md.id_filename, args->verbose);
 	create_mosaik2_database_file_val(md.version_filename, &mdfv, sizeof(mdfv),  args->verbose);
 	create_mosaik2_database_file_val(md.database_image_resolution_filename, &database_image_resolution, sizeof(database_image_resolution), args->verbose);
 	create_mosaik2_database_file_val(md.createdat_filename, &n, md.createdat_sizeof, args->verbose);
 	create_mosaik2_database_file_readme(md.readme_filename, args->verbose);
-
 
 	return 0;
 }
